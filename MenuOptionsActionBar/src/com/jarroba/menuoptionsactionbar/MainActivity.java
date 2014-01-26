8e300b030417b4908fd1da04b4e75a39b9cfeeb8
Ramon <r.invarato@gmail.com>
2014-01-26 18:40:24 +0100

    Subida

---
+++ MenuOptionsActionBar/src/com/jarroba/menuoptionsactionbar/MainActivity.java
@@ -0,0 +1,23 @@
+package com.jarroba.menuoptionsactionbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+}
